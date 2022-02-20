3a906aa8e8a0eba9168589b28698e576dab440c8
m-yashima <mi2756283401@icloud.com>
2022-02-20 08:23:07 +0000

    kadai

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,4 +1,20 @@
-<!DOCTYPE html>
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+
+{{-- admin.blade.phpの@yield('title')に'profile作成'を埋め込む --}}
+@section('title', 'profile作成')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>profile作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection<!DOCTYPE html>
 <html>
     <head>
         <meta charset = "utf-8">
